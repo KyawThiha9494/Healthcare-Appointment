409e2652f7916e037ec0413cdfbf8e053b0f8095
Kyaw Thiha <46520734+KyawThiha9494@users.noreply.github.com>
2024-09-05 17:39:13 +0800

    Initial commit with projects

---
+++ healthcare-appointment-system/src/test/java/com/healthcare_appointment_system/HealthcareAppointmentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.healthcare_appointment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthcareAppointmentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
